f90cb6f25f56fe50123023ca5ba8366ef665cc83
Anduin Xue <anduin@aiursoft.com>
2026-06-19 16:02:01 +0000

    fix: update APT_CONFIG_PACKAGE and APKG_SERVER for development environment

--- mods/05-live-kernel-apps-installer/install.sh
+++ mods/05-live-kernel-apps-installer/install.sh
@@ -46,10 +46,6 @@ apt install $INTERACTIVE \
     --install-recommends
 judge "Install anduinos-desktop"
 
-print_ok "Finalizing dconf database..."
-dconf update
-judge "Final dconf update"
-
 print_ok "Installing AnduinOS installer (Ubiquity + wrapper + slides + bwrap compat)..."
 apt install $INTERACTIVE anduinos-installer-config --no-install-recommends
 judge "Install anduinos-installer-config"
