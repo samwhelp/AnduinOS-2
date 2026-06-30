7e144d9b3864dc509599dcc25a067c36b061207d
samwhelp <samwhelp@users.noreply.github.com>
2026-06-30 12:37:56 +0800

    fix

--- template/engine/libs/controller/path.sh
+++ template/engine/libs/controller/path.sh
@@ -5,8 +5,9 @@
 #=============================
 
 ##
-## gear / libs
-## gear / mods
+## * plan / template / gear
+## * plan / template / gear / libs
+## * plan / template / gear / mods
 ##
 
 GEAR_DIR_PATH="$(realpath "${LIBS_DIR_PATH}/..")"
@@ -19,6 +20,10 @@ MODS_DIR_PATH="${GEAR_DIR_PATH}/mods"
 # Path / Model / Base
 #=============================
 
+##
+## * plan / template
+##
+
 PLAN_DIR_PATH="$(realpath "${GEAR_DIR_PATH}/../..")"
 TEMPLATE_DIR_PATH="$(realpath "${GEAR_DIR_PATH}/..")"
 
@@ -29,8 +34,15 @@ TEMPLATE_DIR_PATH="$(realpath "${GEAR_DIR_PATH}/..")"
 # Path / Model / Skeleton
 #=============================
 
-WORK_DIR_PATH="${PLAN_DIR_PATH}/work"
+##
+## * plan / dist
+## * plan / work
+## * plan / work / img
+## * plan / work / iso
+##
+
 DIST_DIR_PATH="${PLAN_DIR_PATH}/dist"
+WORK_DIR_PATH="${PLAN_DIR_PATH}/work"
 
 DISTRO_IMG_DIR_PATH="${WORK_DIR_PATH}/img"
 DISTRO_ISO_DIR_PATH="${WORK_DIR_PATH}/iso"
@@ -41,6 +53,13 @@ DISTRO_ISO_DIR_PATH="${WORK_DIR_PATH}/iso"
 #=============================
 # Path / Model / Master
 #=============================
+
+##
+## * paln / template / asset
+## * plan / template / asset / overlay
+## * plan / template / asset / package
+## * plan / template / asset / package / install
+##
 
 ASSET_DIR_PATH="${TEMPLATE_DIR_PATH}/asset"
 OVERLAY_DIR_PATH="${ASSET_DIR_PATH}/overlay"
